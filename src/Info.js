4050e6549823439fad5155b3728a7952fa356d9e
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-03-15 23:50:57 +0600

    Add files via upload

--- src/Info.js
+++ src/Info.js
@@ -1,19 +1,27 @@
 import React from 'react';
 const Info = ({ info }) => {
+    //   if (info.age > 20){
+    //     return (
+    //       <div className="info" key= {info.id}>
+    //         <div>Name: { info.name }</div>
+    //         <div>Age: { info.age }</div>
+    //         <div>Dis: { info.dis }</div>
+    //       </div>
+    //     )
+    //   } else {
+    //     return null
+    //   }
+    // });
     const infoList = info.map(info => {
-      if (info.age > 20){
-        return (
-          <div className="info" key= {info.id}>
+      return info.age > 20 ? (
+<div className="info" key= {info.id}>
             <div>Name: { info.name }</div>
-            <div>Age: { info.age }</div>
-            <div>Dis: { info.dis }</div>
-          </div>
-        )
-      } else {
-        return null
-      }
+           <div>Age: { info.age }</div>         
+            <div>Dis: { info.dis }</div>      
+            </div>
+      ) : null;
+    });
 
-    })
     return (
       <div className="info-list">
         { infoList }
